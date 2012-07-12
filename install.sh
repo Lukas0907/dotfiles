7976b95c59dab47da2de1e1c74dadf900acbee74
Lukas Anzinger <lukas@lukasanzinger.at>
2012-07-12 16:53:36 +0200

    Added install.sh script that makes symlinks and so on.

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -ex
+
+git submodule init && git submodule update
+
+# zsh
+ln -snf $PWD/grml/etc-core/etc/zsh/zshrc $HOME/.zshrc
+ln -snf $PWD/_zshrc.local $HOME/.zshrc.local
+ln -snf $PWD/_zshrc.envs $HOME/.zshrc.envs
+ln -snf $PWD/_zshrc.aliases $HOME/.zshrc.aliases
+
+# vim
+ln -snf $PWD/grml/etc-core/etc/vim/vimrc $HOME/.vimrc
+ln -snf $PWD/_vimrc.local $HOME/.vimrc.local
+mkdir -p $HOME/.vim/bundle
+for V in $PWD/vim/*; do
+	ln -snf $V $HOME/.vim/bundle
+done
+
+# solarized dircolors
+ln -snf $PWD/dircolors/dircolors.ansi-light $HOME/.dircolors
+
+# tmux
+ln -snf $PWD/_tmux.conf $HOME/.tmux.conf
+
+# git
+ln -snf $PWD/_gitconfig $HOME/.gitconfig
+
+if ! grep -q "^$USER:.*zsh$" /etc/passwd; then
+	echo "Changing login shell for $USER to zsh ..."
+	chsh -s `which zsh`
+fi
